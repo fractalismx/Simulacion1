5070b6db3f79a3a54f9eb24d6923129d641474e4
hagaren21 <hagaren21@hotmail.com>
2017-03-07 19:07:25 -0600

    actualizacion intervalos

--- esfera.py
+++ esfera.py
@@ -8,22 +8,42 @@ Created on Tue Mar  7 18:08:48 2017
 from random import *
 from pylab import *
 from math import *
+m=1000
+N=10000
 
-N=10000000
-cuenta=0
 f=[]
-
-for i in range(N):
-    x=random()
-    y=random()
-    z=random()
-    
-    if x**2+y**2+z**2<1:
-        cuenta=cuenta+1
-    
-    if i%100:
-        f.append(cuenta)
+for k in range(m):
+    cuenta=0
+    for i in range(N):
+        x=random()
+        y=random()
+        z=random()
         
-hist(f, bins=100)     
+        if x**2+y**2+z**2<1:
+            cuenta=cuenta+1
+    f.append(8*cuenta/N)
     
-print(8*cuenta/N)
+f.sort()
+q=len(f)
+
+li=f[int(0.05*q)]
+ld=f[int(0.95*q)]
+
+print(li,ld)   
+
+sigma=0
+
+for j in range(q):
+    sigma=sigma+(mu-f[j])**2
+  
+sigma=sigma/N
+sigma=sqrt(sigma)
+
+limite_inferior=mu-1.65*sigma
+limite_superior=mu+1.65*sigma
+
+print(limite_inferior)
+print(limite_superior)
+
+print(mu)
+print(q)
